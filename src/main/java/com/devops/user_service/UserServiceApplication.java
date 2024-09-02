a8d20015997eefb4d715553a06aaa17aaf310418
MMomcilovic <miladin.momcilovic@gmail.com>
2024-09-02 22:30:18 +0200

    feat: logging

--- src/main/java/com/devops/user_service/UserServiceApplication.java
+++ src/main/java/com/devops/user_service/UserServiceApplication.java
@@ -1,14 +1,21 @@
 package com.devops.user_service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 @EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		log.info("Welcome home Page " + LocalDateTime.now());
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
